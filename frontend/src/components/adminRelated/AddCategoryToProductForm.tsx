995c9f7877a9cdc4b960d0e19aef7ab202e0537e
MrPanda05 <mr.panda0564@gmail.com>
2025-02-22 20:56:44 -0300

    fixed typo

--- frontend/src/components/adminRelated/AddCategoryToProductForm.tsx
+++ frontend/src/components/adminRelated/AddCategoryToProductForm.tsx
@@ -22,7 +22,7 @@ export default function AddCategoryToProductForm(){
         <div className="rounded-md">
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 text-white font-bold text-2xl/9">
             <h1 className="text-center">
-                Cadastrar produto
+            Adcionar categoria em um produto
             </h1>
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <form method="POST" className="space-y-6" onSubmit={handleSubmit}>
